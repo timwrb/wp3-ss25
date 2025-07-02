227f811699da0e896e7e3eba23a1a7d181da226d
Tim Wurmbrand <wurmbrand.tim@gmail.com>
2025-07-02 17:19:49 +0200

    feat: create "Stock" model in "productmanager" app

--- stockmanager/models.py
+++ stockmanager/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from productmanager.models import Product
 
-# Create your models here.
+class Stock(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    forceInStock = models.BooleanField(default=False)
+    currentStock = models.FloatField(default=0.0)
+    lowestStock = models.FloatField(default=0.0)
+    highestStock = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return f"Stock for {self.product.name}"
